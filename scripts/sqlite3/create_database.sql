eb1dd376039214141f40fdd353f0cff2a0798b35
Evan Stoddard <evan.stoddard@me.com>
2024-07-14 21:11:27 -0500

    Started utility to create sqlite3 users. SQLite3 db updates. Started backend API to create users.

--- scripts/sqlite3/create_database.sql
+++ scripts/sqlite3/create_database.sql
@@ -1,4 +1,11 @@
 CREATE TABLE users(
     uin TEXT NOT NULL,
-    email TEXT NOT NULL
+    email TEXT NOT NULL,
+    hashed_password TEXT NOT NULL
+);
+
+CREATE TABLE challenge_keys(
+    uin TEXT NOT NULL
+    text TEXT NOT NULL
+    FOREIGN KEY(uin) REFERENCES users(uin)
 );
--- src/backends/sqlite3/sqlite3_backend.c
+++ src/backends/sqlite3/sqlite3_backend.c
@@ -25,6 +25,8 @@
 #define SQLITE3_BACKEND_QUERY_USERNAME_STATEMENT    "SELECT * FROM users WHERE uin='%s' LIMIT 1"
 #define SQLITE3_BACKEND_QUERY_EMAIL_STATEMENT       "SELECT * FROM users WHERE email='%s' LIMIT 1"
 
+#define SQLITE3_BACKEND_INSERT_USER_STATEMENT       "INSERT INTO users (uin, email) VALUES('%s', '%s')"
+
 #define SQLITE3_BACKEND_UIN_COL_NAME    "uin"
 #define SQLITE3_BACKEND_EMAIL_COL_NAME  "email"
 
@@ -73,6 +75,36 @@ static backend_ret_t prv_sqlite3_backend_fetch_user_info_with_uin(struct backend
 static backend_ret_t prv_sqlite3_backend_fetch_user_info_with_email(struct backend_t *backend, char *email, user_info_t *user_info);
 
 /**
+ * @brief Function to check if user exists with given UIN
+ * 
+ * @param backend Pointer to backend instance
+ * @param uin UIN to check for
+ * @return true User exists with UIN
+ * @return false User does not exist with UIN
+ */
+static bool prv_sqlite3_backend_user_exists_with_uin(struct backend_t *backend, char *uin);
+
+/**
+ * @brief Function to check if user exists with given email
+ * 
+ * @param backend Pointer to backend instance
+ * @param uin Email to check for
+ * @return true User exists with Email
+ * @return false User does not exist with Email
+ */
+static bool prv_sqlite3_backend_user_exists_with_email(struct backend_t *backend, char *email);
+
+/**
+ * @brief Create user with provided UIN and email
+ * 
+ * @param backend Pointer to backend instance
+ * @param uin UIN of new user
+ * @param email Email address of new user
+ * @return backend_ret_t Status of request
+ */
+static backend_ret_t prv_sqlite_backend_create_user(struct backend_t *backend, char *uin, char *email);
+
+/**
  * @brief SQLite3 callback for fetching user info
  * 
  * @param ctx Context (user info struct)
@@ -90,6 +122,7 @@ static int prv_sqlite_backend_user_info_query_cb(void *ctx, int argc, char **col
 static void prv_sqlite3_backend_connect_api(sqlite3_backend_t *inst) {
     inst->base.api.fetch_user_info_with_uin = prv_sqlite3_backend_fetch_user_info_with_uin;
     inst->base.api.fetch_user_info_with_email = prv_sqlite3_backend_fetch_user_info_with_email;
+    inst->base.api.create_user = prv_sqlite_backend_create_user;
 }
 
 static backend_ret_t prv_sqlite3_backend_fetch_user_info_with_uin(struct backend_t *backend, char *uin, user_info_t *user_info) {
@@ -147,6 +180,93 @@ static backend_ret_t prv_sqlite3_backend_fetch_user_info_with_email(struct backe
     }
     
     return BACKEND_RET_SUCCESS;
+}
+
+static bool prv_sqlite3_backend_user_exists_with_uin(struct backend_t *backend, char *uin) {
+    if (
+        backend == NULL ||
+        uin == NULL
+    ) {
+        return BACKEND_RET_BAD_ARGS;
+    }
+    
+    user_info_t user_info = {0};
+    bool exists = false;
+    
+    // Attempt to get user info with UIN
+    backend_ret_t ret = prv_sqlite3_backend_fetch_user_info_with_uin(backend, uin, &user_info);
+    
+    // Check if there's a backend error
+    if (ret != BACKEND_RET_SUCCESS) {
+        exists = false;
+    }
+    
+    // Check for entries and free allocated memory
+    if (user_info.uin != NULL) {
+        exists = true;
+        free(user_info.uin);
+    }
+    
+    if (user_info.email != NULL) {
+        free(user_info.email);
+    }
+    
+    return exists;
+}
+
+static bool prv_sqlite3_backend_user_exists_with_email(struct backend_t *backend, char *email) {
+    if (
+        backend == NULL ||
+        email == NULL
+    ) {
+        return BACKEND_RET_BAD_ARGS;
+    }
+    
+    user_info_t user_info = {0};
+    bool exists = false;
+    
+    // Attempt to get user info with UIN
+    backend_ret_t ret = prv_sqlite3_backend_fetch_user_info_with_email(backend, email, &user_info);
+    
+    // Check if there's a backend error
+    if (ret != BACKEND_RET_SUCCESS) {
+        exists = false;
+    }
+    
+    // Check for entries and free allocated memory
+    if (user_info.uin != NULL) {
+        exists = true;
+        free(user_info.uin);
+    }
+    
+    if (user_info.email != NULL) {
+        free(user_info.email);
+    }
+    
+    return exists;
+}
+
+static backend_ret_t prv_sqlite_backend_create_user(struct backend_t *backend, char *uin, char *email) {
+    if (
+        backend == NULL || 
+        uin == NULL || 
+        email == NULL
+    ) {
+        return BACKEND_RET_BAD_ARGS;
+    }
+    
+    user_info_t user_info = {0};
+    
+    // Check if user already exists with UIN
+    if (prv_sqlite3_backend_user_exists_with_uin(backend, uin)) {
+        return BACKEND_RET_USER_ALREADY_EXISTS;
+    }
+    
+    // Check if user already exists with email
+    if (prv_sqlite3_backend_user_exists_with_email(backend, email)) {
+        return BACKEND_RET_EMAIL_ALREADY_EXISTS;
+    }
+    
 }
 
 static int prv_sqlite_backend_user_info_query_cb(void *ctx, int argc, char **col_data, char **col_name) {
